307ba3025eca7f8fbdf03293b38d539efcad2a17
Mirrykal2 <kaallokx76@gmail.com>
2025-01-28 10:05:28 +0530

    Create noprefix-pyari.js

---
+++ Priyansh/commands/noprefix-pyari.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "PyariSamjhGayi",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "MrTomXxX", 
+	description: "hihihihi",
+	commandCategory: "no prefix",
+	usages: "began ayee",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("🙈")==0 || event.body.indexOf("🙈🙈")==0 || event.body.indexOf("Baigan")==0 || event.body.indexOf("Began")==0) {
+		var msg = {
+				body: "🤭Pyari Samjh gyi 🫠",
+				attachment: fs.createReadStream(__dirname + `/noprefix/pyari.mp4`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🤫", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
